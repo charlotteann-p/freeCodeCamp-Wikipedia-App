09c21fc70252cb332c8a3f93f21e9861cb0f45ed
Charlotte P <capowning@gmail.com>
2018-05-29 20:38:01 -0400

    Updated version

--- wikiscript.js
+++ wikiscript.js
@@ -1,27 +1,34 @@
+// $(document).ready(function(){
+//   alert("jquery loaded");
+// });
+
 document.getElementById('searchbutton').addEventListener('click', function() {
   var searchtext = document.getElementById('searchbox').value;
+  
+  $("#resultslist").empty();
   
   var api = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch='"+ searchtext +"'";
   var xhr = new XMLHttpRequest();
 
-  xhr.open('GET', api, true);
+  xhr.open ('GET', api, true);
 
   xhr.onload = function() {
     
     var data = JSON.parse(this.response);
     for (var i in data.query.pages) {
       console.log(data.query.pages[i].title);
-      // var searchresult = document.createElement("li");
-      // searchresult.value = data.query.pages[i].title;
-      // document.getElementById("resultslist").appendChild(searchresult); 
       var title = data.query.pages[i].title;
-      title.innerHTML = '<a href="https://wikipedia.org/wiki/"+title+">+title+</a>';
-      document.getElementById("resultslist").append("<li>");
-      document.getElementById("resultslist").append(title);
-      document.getElementById("resultslist").append("</li>");
-
+      var link = "https://en.wikipedia.org/wiki/"+title.replace(/[\s]/g, "_");
+      var tag = "<a href="+link+">"+title+"</a>";
+      var searchresult = document.createElement("li");
+      searchresult.innerHTML = (tag);
+      document.getElementById("resultslist").appendChild(searchresult);
+      //document.getElementById("resultslist").appendChild(tag); 
+     
         }
     }
- xhr.send();
+  xhr.send();
 
 });
+ 
+
